658d41867c643b7d196ec3b8681bc404a0e601c9
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-08-28 13:53:33 -0700

    Delay admin check until after page load

--- app/static/js/adminCheck.js
+++ app/static/js/adminCheck.js
@@ -1,14 +1,14 @@
 (() => {
     const debug = (...args) => window.debugLog('adminCheck.js', ...args);
     debug('Loaded');
-
-    (function() {
-    debug('Checking admin wallet');
-    if (!document.body.classList.contains('admin-wallet')) {
-        debug('Admin wallet not found, redirecting');
-        window.location.replace('/');
-    } else {
-        debug('Admin wallet present');
+    function checkAdmin() {
+        debug('Checking admin wallet');
+        if (!document.body.classList.contains('admin-wallet')) {
+            debug('Admin wallet not found, redirecting');
+            window.location.replace('/');
+        } else {
+            debug('Admin wallet present');
+        }
     }
-    })();
+    window.addEventListener('load', checkAdmin);
 })();
