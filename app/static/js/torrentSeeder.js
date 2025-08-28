5d1648ae38249408b76e656284271b2d4d502ce5
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-08-27 17:05:21 -0700

    Render torrents inline

--- app/static/js/torrentSeeder.js
+++ app/static/js/torrentSeeder.js
@@ -15,6 +15,25 @@
         try {
             client.add(link.href, (torrent) => {
                 console.log('Seeding', torrent.infoHash);
+
+                if (torrent.files && torrent.files.length > 0) {
+                    // Create container to hold rendered content
+                    const container = document.createElement('div');
+                    link.parentNode.insertBefore(container, link);
+
+                    torrent.files[0].appendTo(
+                        container,
+                        { autoplay: true, controls: true },
+                        (err) => {
+                            if (err) {
+                                console.error('Error rendering torrent', link.href, err);
+                            }
+                        }
+                    );
+
+                    // Remove placeholder link once content is rendered
+                    link.remove();
+                }
             });
         } catch (err) {
             console.error('Error adding torrent', link.href, err);
